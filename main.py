6673254dc4954ed8aa40fe6f5285755e471f6128
zahrahijazii <105450781+zahrahijazii@users.noreply.github.com>
2023-08-08 01:17:10 +0400

    Made the game educational by adding a file of the missing states

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import turtle
 import pandas as pd
+import csv
 #set up the screen
 screen = turtle.Screen()
 screen.title("U.S States Game")
@@ -18,7 +19,17 @@ while len(correct_guess) != 50:
 
     state_name = states_data[states_data.state == answer_state]
 
-    if answer_state in states_data.state.values:
+    if answer_state == "Exit":
+        all_states = states_data.state.to_list()
+        missing_states = []
+        for state in all_states:
+            if state not in correct_guess:
+                missing_states.append(state)
+            new_data = pd.DataFrame(missing_states)
+            new_data.to_csv("states_to_learn.csv")
+            break
+
+    elif answer_state in states_data.state.values:
         t.hideturtle()
         t.penup()
         t.goto(int(state_name.x), int(state_name.y))
@@ -26,5 +37,9 @@ while len(correct_guess) != 50:
         correct_guess.append(answer_state)
 
 
-screen.exitonclick()
+
+
+
+
+
 
